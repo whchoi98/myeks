cf6aa77c6fe901edd5317b95fd55d104941f8ec3
whchoi98 <whchoi98@gmail.com>
2022-10-29 00:03:40 +0900

    edit shell - eks_shell.sh - version

--- shell/eks_shell.sh
+++ shell/eks_shell.sh
@@ -29,7 +29,7 @@ echo "export PrivateSubnet03=${PrivateSubnet03}" | tee -a ~/.bash_profile
 
 # eks cluster 환경변수 생성 
 export ekscluster_name="eksworkshop"
-export eks_version="1.21"
+export eks_version="1.22"
 export instance_type="m5.xlarge"
 export public_selfmgmd_node="frontend-workloads"
 export private_selfmgmd_node="backend-workloads"
