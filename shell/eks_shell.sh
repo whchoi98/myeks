1fae2bcbd0990765124fa2f50f972e14aefe8f58
whchoi98 <whchoi98@gmail.com>
2024-02-18 10:48:53 +0900

    edit file - esk_shell.sh

--- shell/eks_shell.sh
+++ shell/eks_shell.sh
@@ -12,7 +12,7 @@ export PublicSubnet03_NAME=eksworkshop-PublicSubnet03
 export PrivateSubnet01_NAME=eksworkshop-PublicSubnet01
 export PrivateSubnet02_NAME=eksworkshop-PublicSubnet02
 export PrivateSubnet03_NAME=eksworkshop-PublicSubnet03
-export VPC_NAME=eksworkshop
+export VPC_NAME="eksworkshop"
 export VPC_ID=$(aws ec2 describe-vpcs --filters "Name=tag:Name,Values=$VPC_NAME" | jq -r '.Vpcs[].VpcId')
 export PublicSubnet01=$(aws ec2 describe-subnets --filters "Name=tag:Name,Values=$PublicSubnet01_NAME" | jq -r '.Subnets[].SubnetId')
 export PublicSubnet02=$(aws ec2 describe-subnets --filters "Name=tag:Name,Values=$PublicSubnet02_NAME" | jq -r '.Subnets[].SubnetId')
@@ -20,7 +20,7 @@ export PublicSubnet03=$(aws ec2 describe-subnets --filters "Name=tag:Name,Values
 export PrivateSubnet01=$(aws ec2 describe-subnets --filters "Name=tag:Name,Values=$PrivateSubnet01_NAME" | jq -r '.Subnets[].SubnetId')
 export PrivateSubnet02=$(aws ec2 describe-subnets --filters "Name=tag:Name,Values=$PrivateSubnet02_NAME" | jq -r '.Subnets[].SubnetId')
 export PrivateSubnet03=$(aws ec2 describe-subnets --filters "Name=tag:Name,Values=$PrivateSubnet03_NAME" | jq -r '.Subnets[].SubnetId')
-echo "export VPC_ID=${vpc_ID}" | tee -a ~/.bash_profile
+echo "export VPC_ID=${VPC_ID}" | tee -a ~/.bash_profile
 echo "export PublicSubnet01=${PublicSubnet01}" | tee -a ~/.bash_profile
 echo "export PublicSubnet02=${PublicSubnet02}" | tee -a ~/.bash_profile
 echo "export PublicSubnet03=${PublicSubnet03}" | tee -a ~/.bash_profile
